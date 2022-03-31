8f0c626ecdfd769332e47989286315e169fa1ca2
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2022-03-31 15:19:01 +0530

    upload_file done

---
+++ jsonfile/src/main/java/com/awss3/jsonfile/JsonfileApplication.java
@@ -0,0 +1,13 @@
+package com.awss3.jsonfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonfileApplication.class, args);
+	}
+
+}
---
+++ jsonfile/src/test/java/com/awss3/jsonfile/JsonfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awss3.jsonfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
